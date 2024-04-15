c4c5c7a4db9227d42ac3761f7b746cdc3e16c235
chao-smile <chaosmile404@gmail.com>
2024-04-15 17:21:33 +0800

    feat: 使用 revalidatePath  重新验证

--- app/actions.ts
+++ app/actions.ts
@@ -2,6 +2,7 @@
 
 import { redirect } from "next/navigation";
 import { addNote, updateNote, delNote } from "@/lib/redis";
+import { revalidatePath } from "next/cache";
 
 export async function saveNote(noteId, title, body) {
   const data = JSON.stringify({
@@ -12,14 +13,17 @@ export async function saveNote(noteId, title, body) {
 
   if (noteId) {
     updateNote(noteId, data);
+    revalidatePath("/", "layout");
     redirect(`/note/${noteId}`);
   } else {
     const res = await addNote(data);
+    revalidatePath("/", "layout");
     redirect(`/note/${res}`);
   }
 }
 
 export async function deleteNote(noteId) {
   delNote(noteId);
+  revalidatePath("/", "layout");
   redirect("/");
 }
